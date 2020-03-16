cfc881f108aec4846b11175c9934fcb03c602fef
Helen <helensmyrnova@gmail.com>
2020-03-16 18:04:08 +0200

    додавання інших секцій

--- js/main.js
+++ js/main.js
@@ -26,3 +26,32 @@ $(document).ready(function(){
         });
   });
 
+// Плавне переміщення до каталогу
+  let catalog = document.querySelector('.catalog');
+  
+  catalog.addEventListener('click', function(event){
+      event.preventDefault(); //отмена стандартного обработчика
+      let blockId = catalog.getAttribute('href');
+      document.querySelector(''+blockId).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+      })
+  });
+
+
+// Button up
+let anchor = document.querySelector('.go-top');
+
+anchor.addEventListener('click', function(event){
+    event.preventDefault(); //отмена стандартного обработчика
+    let blockId = anchor.getAttribute('href');
+    document.querySelector(''+blockId).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+});
+
+// Делаем непрозначную / прозрачную кнопку
+window.addEventListener('scroll', function(){
+    anchor.hidden = (this.pageYOffset < document.documentElement.clientHeight)
+});
